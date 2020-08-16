d6a2eed8b4b93213ceb7b8bf9ac483507238262e
ARAVIND <aravind-15>
2020-08-16 19:18:34 +0530

    asdf

--- backend/library/config.js
+++ backend/library/config.js
@@ -2,5 +2,5 @@ const { mongo } = require("mongoose");
 
 module.exports={
     port: process.env.PORT || 5000,
-    db_connectionstring : process.env.CONNECTION_STRING ||
+    db_connectionstring : process.env.CONNECTION_STRING ||'mongodb://localhost:27017//userDB'
 }
