7ffa5f3ef9fadda3dc7ee0accb65d5342a85e115
ARAVIND <aravind-15>
2020-08-16 22:47:52 +0530

    jsddd

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -41,9 +41,9 @@ $(function(){
 
 
 function display(data){
-    var txt="<table><tr><th>ID</th><th>NAME</th><th>AGE</th><th>MOBILE</th></tr>";
+    var txt="<table><tr><th>ROLL NO</th><th>NAME</th><th>AGE</th><th>MOBILE</th></tr>";
      for(var i=0;i<data.length;i++){
-         txt+="<tr><td>"+data[i].id+"</td><td>"+data[i].name+"</td><td>"+data[i].age+"</td><td>"+data[i].mobile+"</td"+"</tr>";
+         txt+="<tr><td>"+data[i].roll no+"</td><td>"+data[i].name+"</td><td>"+data[i].age+"</td><td>"+data[i].mobile+"</td"+"</tr>";
      }
      txt+="</table>";
      $("#table").html(txt);
